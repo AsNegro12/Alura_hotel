0cc7c8d644dedaca869ce53b7031c495246f92ea
UrielPCDesk <floresruizur13l@outlook.com>
2023-09-20 09:21:17 -0700

    Se creo el repositorio

---
+++ src/mx/com/aluraHotel/Controller/HuespedesController.java
@@ -0,0 +1,5 @@
+package mx.com.aluraHotel.Controller;
+
+public class HuespedesController {
+
+}
---
+++ src/mx/com/aluraHotel/viws/Login.java
@@ -0,0 +1,10 @@
+package mx.com.aluraHotel.viws;
+
+import javax.swing.JFrame;
+
+public class Login extends JFrame
+{
+	public static void main(String[] args) {
+		
+	}
+}
